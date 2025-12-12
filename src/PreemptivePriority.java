c7bab527d32d4cd535c0078f1fb6736b9d13b910
Abdallah Kareem <162487911+abdallahkareem@users.noreply.github.com>
2025-12-12 14:55:36 +0200

    Add preemptive priority scheduling basis
    Implemented preemptive priority scheduling logic in the run method.

--- src/PreemptivePriority.java
+++ src/PreemptivePriority.java
@@ -4,7 +4,33 @@ public class PreemptivePriority implements Scheduler {
 
 	@Override
 	public void run(List<Process> processes) {
-		// TODO Auto-generated method stub
+		System.out.println("====== Preemptive Priority ======");
+		int time = 0;
+		
+		int completed = 0; // Number of completed processes
+		
+		int n = processes.size();
+		for(Process p : processes) {
+			p.remaining = p.burst; // intialize all remainings as bursts 
+		}
+		
+		while(completed < n) {
+			Process current = null;
+			for(int i = 0;i < n;i++) {
+				Process p = processes.get(i);
+				if(p.arrival <= time && p.remaining > 0) {
+					if(current == null) {
+						current = p;
+					}
+					else {
+						if(p.priority < current.priority) {
+							current = p;
+						}
+					}
+				}
+			}
+			
+		}
 		
 	}
 
