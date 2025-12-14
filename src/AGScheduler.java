b303717b2295cd0e2832bf5d0d6cd8efb8e1d627
Ahmed Mohamed <omoahmed54321@gmail.com>
2025-12-14 18:50:48 +0200

    added the logic for the AG schedular

--- src/AGScheduler.java
+++ src/AGScheduler.java
@@ -1,11 +1,100 @@
+import java.util.LinkedList;
 import java.util.List;
-
+import java.util.Queue;
 public class AGScheduler implements Scheduler { // Hybrid Algorithm (FCFS, Priority_P, SRTF)
-
 	@Override
 	public void run(List<Process> processes) {
-		// TODO Auto-generated method stub
-		
-	}
+		int completed = 0;
+		int time = 0;
+		int n = processes.size();
 
+		// initilize the remaining time to be equal the burst time
+		for (Process process : processes) {
+			process.remaining = process.burst;
+		}
+
+		Queue<Process> ready_queue = new LinkedList<>();
+		while (completed < n) {
+			for (Process p : processes) {
+				if (p.remaining != 0 && p.arrival <= time && !ready_queue.contains(p)) {
+					ready_queue.add(p);
+				}
+			}
+
+			// if ready queue is empty then we finished processes
+			if (ready_queue.isEmpty()) {
+				time++;
+				continue;
+			}
+			Process current = ready_queue.poll();
+			int current_quantum = current.quantum;
+			
+			// for the first 25% of the quantum we will serve as FCFS
+			int quarter_quantum = (int) Math.ceil(current_quantum*0.25);
+			for (int i = 0; i < quarter_quantum && current.remaining > 0; i++) {
+				current.remaining--;
+				time++;
+			}
+			if (current.remaining == 0) {
+				completed++;
+				current.quantum = 0;
+				continue;
+			}
+
+			// for the second 25% = (50% - 25%) we will work as preemtive priority
+			int second_quarter_quantum = (int) Math.ceil(current_quantum*0.5) - quarter_quantum;
+			Process higher_priority_process = current;
+			// find the highr priority process
+			for (Process process : ready_queue) {
+				if (process.priority < higher_priority_process.priority) {
+					higher_priority_process = process;
+				}
+			}
+			if (higher_priority_process != current) {
+				double remain_priority_quantum = (current_quantum - (second_quarter_quantum + quarter_quantum))/2;
+				current.quantum = current_quantum + (int) Math.ceil(remain_priority_quantum/2);
+				ready_queue.add(current);
+				continue;
+			}
+			for (int i = 0; i < second_quarter_quantum && current.remaining > 0; i++) {
+				current.remaining--;
+				time++;
+			}
+			if (current.remaining == 0) {
+				completed++;
+				current.quantum = 0;
+				continue;
+			}
+
+			// for the rest quantum time we will work as SJF
+			int remainingQuantum = current_quantum - (quarter_quantum + second_quarter_quantum);
+			while (remainingQuantum > 0) {
+				Process shortest_job_process = current;
+				for (Process process : ready_queue) {
+					if (process.remaining < shortest_job_process.remaining) {
+						shortest_job_process = process;
+					}
+				}
+				if (shortest_job_process != current) {
+					current.quantum = current_quantum + remainingQuantum;
+					ready_queue.add(current);
+					break;
+				}
+				for (int i = 0; i < remainingQuantum && current.remaining > 0; i++) {
+					current.remaining--;
+					time++;
+				}
+				if (current.remaining == 0) {
+					completed++;
+					current.quantum = 0;
+					break;
+				}
+			}
+			// if the process finished it's quantum and did't finish it's processes then add 2 to the quantum
+			if (current.remaining > 0 && !ready_queue.contains(current)) {
+				current.quantum += 2;
+				ready_queue.add(current);
+			}
+		}
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List processes = new ArrayList<>();
+		Process process1 = new Process("p1", 0, 17, 4, 7);
+		Process process2 = new Process("p2", 2, 6, 7, 9);
+		Process process3 = new Process("p3", 5, 11, 3, 4);
+		Process process4 = new Process("p4", 15, 4, 6, 6);
+		processes.add(process1);
+		processes.add(process2);
+		processes.add(process3);
+		processes.add(process4);
+		AGScheduler scheduler = new AGScheduler();
+		scheduler.run(processes);
 	}
 
 }
